f85491510af00ae1b7fe5d0b3a93d3dc19a6ee6c
Deivid21 <david.parra.ignacio@gmail.com>
2025-03-23 23:56:03 -0300

    msm8998-common: Build MotoLiveWallpaper3 from prebuilt source
    Change-Id: I9752048563f0c5cea291de544ec7770777a6a2d8

--- common.mk
+++ common.mk
@@ -195,6 +195,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+
+# Moto Live Wallpapers 3
+$(call inherit-product, vendor/motorola/MotoLiveWallpaper3/motolivewallpaper3.mk)
+
 # MotoActions
 PRODUCT_PACKAGES += \
     MotoActions \
